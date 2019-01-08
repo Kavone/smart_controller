13cb0f0bc161f993abb8806d199b7bed27e71e8d
John Fredine <jfredine@hotmail.com>
2019-01-07 21:54:11 -0800

    Increase timer interrupt granularity
    Timer interrupt is now "scheduled" through rewrites of the compare
    register value.

--- src/main.cpp
+++ src/main.cpp
@@ -40,101 +40,96 @@ const int E0_STEP_PIN = 26, E0_DIR_PIN = 28, E0_ENABLE_PIN = 24, E0_CS_PIN = 42;
 const int E1_STEP_PIN = 36, E1_DIR_PIN = 34, E1_ENABLE_PIN = 30, E1_CS_PIN = 44;
 #endif
 
-const int button_check_interval = 1;
-const int debounce_stable_count = 5;
+const int debounce_stable_count = 4;
 volatile int pos;
 volatile int increments;
 volatile int decrements;
+volatile long unsigned total_isr_count;
+volatile long unsigned total_isr_time;
+#define OCR0A_INCR 64
+
 
 /*
- * TIMER0_COMPA_vect and TIMER0_COMPB_vect
+ * TIMER0_COMPA_vect
  * ISR triggered by timer0 where inputs are read and debounced.
  */
-#ifdef DUAL_INTERRUPTS
-ISR(TIMER0_COMPB_vect, ISR_ALIASOF(TIMER0_COMPA_vect));
-#endif
 ISR(TIMER0_COMPA_vect) {
-    static int isr_count;
-
     static int last_a = 1;
     static int curr_a = 1;
     static int curr_a_stable_count;
 
     static int initial_b;
 
-    isr_count++;
+    unsigned long isr_start_time;
 
-    if (isr_count == button_check_interval) {
-        isr_count = 0;
+    isr_start_time = micros();
+    total_isr_count++;
+    OCR0A = (OCR0A + OCR0A_INCR) & 0xff;
 
-        // First deal with the rotary encoder
-        // This is done by checking the value of button B when
-        // a rising edge on button A is found.  A rising edge on
-        // A when B is low indicates clockwise motion while a rising
-        // edge on A when B is high indicates counter-clockwise motion
+    // First deal with the rotary encoder
+    // This is done by checking the value of button B when
+    // a rising edge on button A is found.  A rising edge on
+    // A when B is low indicates clockwise motion while a rising
+    // edge on A when B is high indicates counter-clockwise motion
 
-        // get value of B the first time we detect a change on A
-        // we assume B is stable by the time A begins to change
-        int val = digitalRead(BTN_ENA);
-        if ((curr_a_stable_count == 0) && (val != last_a)) {
-            initial_b = digitalRead(BTN_ENB);
+    // get value of B the first time we detect a change on A
+    // we assume B is stable by the time A begins to change
+    int val = digitalRead(BTN_ENA);
+    if ((curr_a_stable_count == 0) && (val != last_a)) {
+        initial_b = digitalRead(BTN_ENB);
+    }
+
+    // debounce A
+    if (val != curr_a) {
+        curr_a = val;
+        curr_a_stable_count = 1;
+    } else {
+        if ((curr_a_stable_count > 0) &&
+            (curr_a_stable_count < debounce_stable_count)) {
+            curr_a_stable_count++;
         }
+    }
 
-        // debounce A
-        if (val != curr_a) {
-            curr_a = val;
-            curr_a_stable_count = 1;
-        }
-        else {
-            if ((curr_a_stable_count > 0)
-                  && (curr_a_stable_count < debounce_stable_count)) {
-                curr_a_stable_count++;
+    // if values are stable and rising edge on A, update position
+    if (curr_a_stable_count == debounce_stable_count) {
+        if ((last_a == 0) && (curr_a == 1)) {
+            if (initial_b == 0) {
+                increments++;
+                pos++;
+            } else {
+                decrements++;
+                pos--;
             }
-            else {
-                curr_a_stable_count = 0;
+            if (pos < 0) {
+                pos += 20;
+            } else if (pos > 19) {
+                pos -= 20;
             }
         }
 
-        // if values are stable and rising edge on A, update position
-        if (curr_a_stable_count == 0)  {
-            if ((last_a == 0) && (curr_a == 1)) {
-                if (initial_b == 0) {
-                    increments++;
-                    pos++;
-                }
-                else {
-                    decrements++;
-                    pos--;
-                }
-                if (pos < 0) {
-                    pos += 20;
-                }
-                else if (pos > 19) {
-                    pos -= 20;
-                }
-            }
+        curr_a_stable_count = 0;
+        last_a = curr_a;
+    }
 
-            last_a = curr_a;
-        }
-
-        // Now deal with the button press which simply resets the
-        // count to zero so we don't care about bounces
-        val = digitalRead(BTN_ENC);
-        if (val == 0) {
-            pos = 0;
-            increments = 0;
-            decrements = 0;
-        }
+    // Now deal with the button press which simply resets the
+    // count to zero so we don't care about bounces
+    val = digitalRead(BTN_ENC);
+    if (val == 0) {
+        pos = 0;
+        increments = 0;
+        decrements = 0;
+    }
 
 #ifdef __AVR_ATmega2560__
-        val = digitalRead(RESET_SW);
-        if (val == 0) {
-            pos = 0;
-            increments = 0;
-            decrements = 0;
-        }
-#endif
+    val = digitalRead(RESET_SW);
+    if (val == 0) {
+        pos = 0;
+        increments = 0;
+        decrements = 0;
     }
+#endif
+
+    total_isr_time += (micros() - isr_start_time);
 }
 
 
@@ -163,21 +158,26 @@ void setup() {
     pinMode(RESET_SW, INPUT_PULLUP);
 #endif
 
-    // set up a recuring timer interrupt at 1ms
+    // set up a recuring timer interrupt
     cli();                  // disable interrupts
-    OCR0A = 0x81;           // set timer0/A to generate output when it hits 0x80
-                            // pretty much anything is fine here as it will simply
-                            // cause output once every time through the counter
-                            // 0 -> FF sequence which takes 1024us
+
+    TCCR0A &= ~((1 << WGM01) | (1 << WGM00));  // Turn off PWM modes
+    TCCR0B &= ~(1 << WGM02);
+
+    OCR0A = 1;              // set timer0/A to generate output when it
+                            // hits this value.  The ISR will adjust this
+                            // value to to potentially produce multiple
+                            // interrupts during the normal 0 -> FF count
+                            // sequence which takes 1024us
     TIMSK0 |= _BV(OCIE0A);  // Set the compare A output to cause an interrupt
-#ifdef DUAL_INTERRUPTS
-    OCR0B = 0x01;           // set timer0/B to generate output when it hits 0x01
-    TIMSK0 |= _BV(OCIE0B);  // Set the compare B output to cause an interrupt
-#endif
+
     sei();                  // enable interrupts
 }
 
 void loop() {
+    static unsigned loop_count;
+    loop_count++;
+
 #ifdef __AVR_ATmega2560__ 
     static int last_pos = 0;
     if (last_pos < pos) {
@@ -212,5 +212,18 @@ void loop() {
     lcd.setCursor(0,2);
     lcd.print("Decrements: ");
     lcd.print(decrements);
-    delay(10);
+
+    if (loop_count == 100) {
+        loop_count = 0;
+        Serial.print("Count: ");
+        Serial.print(total_isr_count);
+        Serial.print("\n");
+        Serial.print("ISR Time: ");
+        Serial.print(total_isr_time);
+        Serial.print("\n");
+        Serial.print("Wall Time: ");
+        Serial.print(micros());
+        Serial.print("\n\n");
+    }
+    delay(100);
 }
